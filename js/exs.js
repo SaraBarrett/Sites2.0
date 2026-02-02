f1d3d7b8506dc60fb7a09764858aa63b6ebd8ac8
SaraBarrett <sara.c.mon@gmail.com>
2026-02-02 10:57:37 +0000

    objectos

--- js/exs.js
+++ js/exs.js
@@ -1,11 +1,33 @@
 //pp 19
 
-function constWord(){
- 
-    let word= "skateboard";
- 
-    facialHair= word.slice(5).replace ("o","e");
- 
-    console.log (facialHair);
- 
+function constWord() {
+  let word = "skateboard";
+
+  facialHair = word.slice(5).replace("o", "e");
+
+  console.log(facialHair);
+}
+
+//pp 24
+function helloUser() {
+  let firstName = prompt("qual é o teu primeiro nome?");
+  let lastName = prompt("qual é o teu último nome?");
+
+  alert(`Olá ${firstName} ${lastName}!`);
+}
+
+//pp 33
+function productInfo() {
+  let product = {
+    name: "caneta",
+    price: 1.99,
+    inStock: true,
+    colors: ["vermelho", "azul", "verde"],
+  };
+
+  alert("o preço é " + product.price);
+  alert("a cor é " + product.colors[2]);
+
+  product.price = 2.55;
+  alert("o preço é " + product.price);
 }
--- js/script.js
+++ js/script.js
@@ -5,15 +5,39 @@ function helloWorld() {
   //faz console.log
   //console.log("Olá Mundo!");
 
-    //fazer um prompt
+  //fazer um prompt
 
-    let userMood = prompt("Como estás hoje?");
+  let userMood = prompt("Como estás hoje?");
 }
 
-function stringFunction(){
-    let userName = prompt('qual é o teu nome completo?');
-    let shortName = userName.slice(0,userName.indexOf(' '));
+function stringFunction() {
+  let userName = prompt("qual é o teu nome completo?");
+  let shortName = userName.slice(0, userName.indexOf(" "));
 
-    alert('olá ' + shortName);
+  console.log("teste");
+  alert(`olá ${shortName}!`);
+}
 
+function multimediaInfo() {
+  // let courseName = "Técnico de Produtos Multimédia";
+  // let couseHours = 1500;
+
+  let course = {
+    name: "Técnico de Produtos Multimédia",
+    hours: 1500,
+    responsible: "Cristina",
+    students: ['Luis', 'Vanessa', 'Felisberto']
+  };
+
+  alert("o nome do curso é " + course.name);
+  alert("o curso tem " + course.hours + "h");
+  alert("o/a responsável do curso é " + course.responsible);
+
+  let userEdit = prompt("pretende alterar as horas? s/n");
+  if (userEdit == "s") {
+    let userHours = prompt("quais são as novas horas?");
+    course.hours = userHours;
+  }
+  alert("o curso tem " + course.hours + "h");
+  alert("o primeiro aluno a ser inscrito foi o "+course.students[0])
 }
